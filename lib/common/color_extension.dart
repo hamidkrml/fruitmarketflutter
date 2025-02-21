ab918191ddf5d16aa5d8c4b6478e1890dddb5345
hamidkrml <karimlihamid4@gmail.com>
2025-02-21 17:33:55 +0300

    Add color extension utility for consistent color management

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorExtension {
+  static Color get primary => Color(0xff69A03A);
+
+  static Color get secondary => Color(0xffCC7D00);
+
+  static Color get primarytext => Color(0xff141313);
+
+  static Color get secondarytext => Color(0xff989898);
+
+  static Color get placeholder => Color(0xff909090);
+
+  static Color get bg => Colors.white;
+
+  static Color get active => Color(0xff69A03A);
+
+  static Color get inactive => Color(0xff7B7B7B);
+}
